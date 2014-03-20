95979d76528421f83b8d8ee7bc0ca22e654b18f4
Kimberly Narine <kimberlyonarine@gmail.com>
2014-03-20 14:09:22 -0400

    update html, add css

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -9,14 +9,16 @@ class HomeController < ApplicationController
     @data = []
     
     @timeoff = 5
-    @current = nil
+    @current_unit = nil
+    @name = nil
     
     if params["name"]
-      @message = params["name"]["first"].capitalize + " " + params["name"]["last"].capitalize + " not found."
-      for row in 1..@ws.num_rows
+      @name = params["name"]["first"].capitalize + " " + params["name"]["last"].capitalize 
+      @message = @name + " not found."
+      for row in 2..@ws.num_rows
         if @ws[row, 1].downcase == params["name"]["first"].downcase && @ws[row, 2].downcase == params["name"]["last"].downcase 
           @message = nil
-          @current = @ws[row, 6].to_i + 1
+          @current_unit = @ws[row, 6].to_i + 1
           for col in 3..@ws.num_cols
             @data << @ws[1,col] if (col-3)% 4 == 0 
             @data << @ws[row,col]
